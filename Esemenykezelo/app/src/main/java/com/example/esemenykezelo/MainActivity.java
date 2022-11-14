faa08229ac4fd1ad8b39221056e851e7810d5a59
rcfr84 <valcsi20010923@gmail.com>
2022-11-14 18:39:40 +0100

    Eseménykezelés elkezdése.

--- Esemenykezelo/app/src/main/java/com/example/esemenykezelo/MainActivity.java
+++ Esemenykezelo/app/src/main/java/com/example/esemenykezelo/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.CalendarView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int currentYear = 0;
+    private int currentMonth = 0;
+    private int currentDay = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         final TextView selectedDay = findViewById(R.id.selectedDay);
         final TextView selectedMonth = findViewById(R.id.selectedMonth);
         final TextView selectedYear = findViewById(R.id.selectedYear);
+
+        final List<string> calanderStrings = new ArrayList<>();
+        int days = new int[30];
+        final EditText textInput = findViewById(R.id.textInput);
+        final View dayContent = findViewById(R.id.dayContent);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
         {
             @Override
@@ -25,7 +33,30 @@ public class MainActivity extends AppCompatActivity {
                 selectedDay.setText("Nap: " + dayOfMonth);
                 selectedMonth.setText("Hónap: " + month);
                 selectedYear.setText("Év: " + year);
+                currentYear = year;
+                currentMonth = month;
+                currentDay = dayOfMonth;
+                if(dayContent.getVisibility()==View.GONE){
+                    dayContent.setVisibility(View.VISIBLE);
+                }
+                if(currentDay==days[0]){
+                    textInput.setText(calanderStrings.get(0));
+                }
+
             }
         });
+
+        final Button saveTextButton = finViewById(r.id.saveTextButton);
+
+
+        saveTextButton.setOnClickListener( new View.OnClickListener()){
+            @Override
+            public void onClick(View v){
+                days[0]=currentDay;
+                calanderStrings.add(textInput.getText().toString());
+                textInput.setText("");
+            }
+
+        }
     }
 }
