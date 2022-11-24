9b6efd68ca29d5ee81a10c9b32c62e1d13cd8ab7
rcfr84 <valcsi20010923@gmail.com>
2022-11-24 16:15:23 +0100

    Konstruktor létrehozása az event osztályban.

--- Esemenykezelo/app/src/main/java/com/example/esemenykezelo/Event.java
+++ Esemenykezelo/app/src/main/java/com/example/esemenykezelo/Event.java
@@ -3,4 +3,11 @@ package com.example.esemenykezelo;
 public class Event {
     String event, time, date, month, year;
 
+    public Event(String event, String time, String date, String month, String year) {
+        this.event = event;
+        this.time = time;
+        this.date = date;
+        this.month = month;
+        this.year = year;
+    }
 }
