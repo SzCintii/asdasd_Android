0aee8ed03b5cd10eaa30cd30cd47909641fb7ddd
rcfr84 <valcsi20010923@gmail.com>
2022-11-24 16:16:12 +0100

    Getterek és setterek létrehozása az event osztályban.

--- Esemenykezelo/app/src/main/java/com/example/esemenykezelo/Event.java
+++ Esemenykezelo/app/src/main/java/com/example/esemenykezelo/Event.java
@@ -10,4 +10,44 @@ public class Event {
         this.month = month;
         this.year = year;
     }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 }
