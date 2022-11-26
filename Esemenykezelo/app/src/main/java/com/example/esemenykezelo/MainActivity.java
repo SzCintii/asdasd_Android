a158b20327f71cd1da72069c6836a0c3f923ff54
rcfr84 <valcsi20010923@gmail.com>
2022-11-26 11:05:08 +0100

    OpenAddEventActivity metódus megírása.

--- Esemenykezelo/app/src/main/java/com/example/esemenykezelo/MainActivity.java
+++ Esemenykezelo/app/src/main/java/com/example/esemenykezelo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.esemenykezelo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -83,5 +84,10 @@ public class MainActivity extends AppCompatActivity {
                 textInput.setText("");
             }
         });
+    }
+    public void OpenAddEventActivity()
+    {
+        Intent intent = new Intent(this, AddEventActivity.class);
+        startActivity(intent);
     }
 }
